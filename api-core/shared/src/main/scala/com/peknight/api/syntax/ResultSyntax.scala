edb57041720cea11ab6837f0e467f61722ebf3c0
peknight <JKpeknight@gmail.com>
2025-03-11 16:33:45 +0800

    update

--- api-core/shared/src/main/scala/com/peknight/api/syntax/ResultSyntax.scala
+++ api-core/shared/src/main/scala/com/peknight/api/syntax/ResultSyntax.scala
@@ -4,12 +4,12 @@ import cats.ApplicativeError
 import cats.syntax.functor.*
 import com.peknight.api.Result
 import com.peknight.error.Error
-import com.peknight.error.syntax.applicativeError.asError as applicativeErrorAsError
+import com.peknight.error.syntax.applicativeError.asError
 
 trait ResultSyntax:
   extension [F[_], Res[A] <: Result[A], A](fra: F[Res[A]])
-    def asError(using ApplicativeError[F, Throwable]): F[Either[Error, A]] =
-      fra.applicativeErrorAsError.map(_.flatMap(_.ior.toEither))
+    def fResultLiftET(using ApplicativeError[F, Throwable]): F[Either[Error, A]] =
+      fra.asError.map(_.flatMap(_.ior.toEither))
   end extension
 end ResultSyntax
 object ResultSyntax extends ResultSyntax
