30f00fe515498b9165220fda1f1902c58f5afcba
peknight <JKpeknight@gmail.com>
2025-04-15 10:23:50 +0800

    update

--- api-instances/codec/shared/src/main/scala/com/peknight/api/codec/instances/ResultInstances.scala
+++ api-instances/codec/shared/src/main/scala/com/peknight/api/codec/instances/ResultInstances.scala
@@ -1,16 +1,50 @@
 package com.peknight.api.codec.instances
 
 import cats.Applicative
-import com.peknight.error.{Error, Success}
-import com.peknight.codec.sum.{BooleanType, NullType, ObjectType}
 import cats.data.Ior
-import com.peknight.api.Result
 import cats.syntax.option.*
+import com.peknight.api.Result
 import com.peknight.api.pagination.Pagination
 import com.peknight.codec.cursor.Cursor
+import com.peknight.codec.sum.{BooleanType, NullType, ObjectType}
 import com.peknight.codec.{Codec, Decoder, Encoder}
+import com.peknight.error.{Error, Success}
 
 trait ResultInstances:
+  def encodeResult[F[_], S, P <: Pagination, E <: Error, A, Res <: Result[P, A]](
+    successLabel: String = "success",
+    dataLabel: String = "data",
+    errorLabel: String = "error",
+    paginationLabel: String = "pagination"
+  )(mapError: Error => E)(
+    using
+    Applicative[F], ObjectType[S], NullType[S], BooleanType[S], Encoder[F, S, P], Encoder[F, S, E], Encoder[F, S, A]
+  ): Encoder[F, S, Res] =
+    Encoder.forProduct[F, S, Res, (Option[Boolean], Option[A], Option[E], Option[P])](
+      (successLabel, dataLabel, errorLabel, paginationLabel)
+    )(result => (result.success.some, result.data, result.error.map(mapError), result.pagination))
+
+  def decodeResult[F[_], S, P <: Pagination, E <: Error, A, Res <: Result[P, A]](
+    successLabel: String = "success",
+    dataLabel: String = "data",
+    errorLabel: String = "error",
+    paginationLabel: String = "pagination"
+  )(mapError: Error => E)(f: (E Ior A, Option[P]) => Res)(
+    using
+    Applicative[F], ObjectType[S], NullType[S], BooleanType[S],
+    Decoder[F, Cursor[S], P],
+    Decoder[F, Cursor[S], E],
+    Decoder[F, Cursor[S], A]
+  ): Decoder[F, Cursor[S], Res] =
+    Decoder.forProductMap[F, S, Res, (Option[Boolean], Option[A], Option[E], Option[P])](
+      (successLabel, dataLabel, errorLabel, paginationLabel)
+    ) {
+      case (_, Some(data), Some(error), pagination) => f(Ior.both(error, data), pagination)
+      case (_, Some(data), _, pagination) => f(Ior.right(data), pagination)
+      case (_, _, Some(error), pagination) => f(Ior.left(error), pagination)
+      case (_, _, _, pagination) => f(Ior.left(mapError(Success)), pagination)
+    }
+
   def codecResult[F[_], S, P <: Pagination, E <: Error, A, Res <: Result[P, A]](
     successLabel: String = "success",
     dataLabel: String = "data",
@@ -23,13 +57,9 @@ trait ResultInstances:
     Encoder[F, S, E], Decoder[F, Cursor[S], E],
     Encoder[F, S, A], Decoder[F, Cursor[S], A]
   ): Codec[F, S, Cursor[S], Res] =
-    Codec.forProductMap[F, S, Res, (Option[Boolean], Option[A], Option[E], Option[P])](
-      (successLabel, dataLabel, errorLabel, paginationLabel)
-    )(result => (result.success.some, result.data, result.error.map(mapError), result.pagination)) {
-      case (_, Some(data), Some(error), pagination) => f(Ior.both(error, data), pagination)
-      case (_, Some(data), _, pagination) => f(Ior.right(data), pagination)
-      case (_, _, Some(error), pagination) => f(Ior.left(error), pagination)
-      case (_, _, _, pagination) => f(Ior.left(mapError(Success)), pagination)
-    }
+    Codec[F, S, Cursor[S], Res](using
+      encodeResult[F, S, P, E, A, Res](successLabel, dataLabel, errorLabel, paginationLabel)(mapError),
+      decodeResult[F, S, P, E, A, Res](successLabel, dataLabel, errorLabel, paginationLabel)(mapError)(f),
+    )
 end ResultInstances
 object ResultInstances extends ResultInstances
